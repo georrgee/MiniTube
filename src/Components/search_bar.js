45a4f7933bb60f55bf6db911b0f4092de8515796
George Garcia <garcia.george.b@gmail.com>
2020-03-16 09:57:10 -0700

    First Commit

---
+++ src/Components/search_bar.js
@@ -0,0 +1,49 @@
+// Search Bar Component
+import React, { Component } from 'React'; // For component, its still the same thing as const component = React.Component
+// defining component
+
+class SearchBar extends Component { // Plain JS Object
+
+	constructor(props) { // define state in a class based components 
+		super(props);
+		this.state = { term: ''}; // term is an empty string
+	}
+
+	render() { // defining methods within a class
+		return (
+			<div> 
+				<input 
+					value 	 = {this.state.term}
+					onChange = { event => this.setState({ term: event.target.value }) } 
+				/> 
+			</div>
+		);
+	}
+}
+
+
+export default SearchBar;
+
+
+
+
+
+
+/// const SearchBar = () => { // Function
+// 	return <input />; // generate HTML input
+// }
+
+
+//functional components do not have state, only class based components do
+// All JS classes have a special function called Constructor
+// Constructor: first and only function called automatically whenever a new instance of the class is created
+
+
+//this.state.term = event.target.value ----> Dont do this, BAD!
+
+
+
+// 358. Controlled Conponents
+
+// Controlled Component: has its value set by state, so its value only ever changes when the state changes
+// 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+
+import React, { Component } from 'react'; // go find the library called React, install in my application as dependency (installed in the node_modules), and assigned to the variable React
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './Components/search_bar';
+import VideoList from './Components/video_list';
+import VideoDetail from './Components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const YOUTUBE_DATA_API_KEY = "AIzaSyA6sKJIgo5mweN7mUszD-PMxppZcqwwbo0";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component { // => E-6 Syntax (=>): instead of using the keyword function
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] }; // list of videos
+
+		YTSearch ({key: YOUTUBE_DATA_API_KEY, term: 'Nikes on my feet'}, (videosData) => {
+			this.setState({ videos: videosData});
+		});
+	}
+
+	render() { 
+		return ( 
+			<div> 
+				<SearchBar /> 
+				<VideoDetail video = {this.state.videos[0]} />
+				<VideoList videos = { this.state.videos } /> 
+			</div>
+		); 
+	}
+}
+
+/// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+//this.SetState({videos}) ---> works only if the key and the var name is the same naming convention
+
